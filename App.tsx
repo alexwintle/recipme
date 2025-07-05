16adfbe27e64752615886060bfda8838aadcf636
Alex Wintle <alexwintle663@gmail.com>
2025-07-05 23:11:51 +0100

    refactor: update env var to expo format

--- App.tsx
+++ App.tsx
@@ -3,21 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 import { AuthProvider } from './src/contexts/AuthContext';
 
 export default function App() {
-  return (
-    // <AuthProvider>
-        <View style={styles.container}>
-            <Text>Welcome to Recipme!</Text>
-            <StatusBar style="auto" />
-        </View>
-    // </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <View style={styles.container}>
+                <Text>Welcome to Recipme!!!!!</Text>
+                <StatusBar style="auto" />
+            </View>
+        </AuthProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
